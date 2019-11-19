a224a8aa5edc585f43d1c4e65c6a6d1820307054
Guillermo Villalta <mexcelus@gmail.com>
2019-11-18 23:44:55 -0700

    navbar adj

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,16 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <div id="navbarNav">
+            <ul class="nav">
+                <li class="nav-item navbar">
+                    <a class="nav-link" href="#">Github</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <button class="btn btn-success">
+                        <a class="nav-link text-white" href="#">Sign In</a>
+                    </button>
                 </li>
             </ul>
         </div>
