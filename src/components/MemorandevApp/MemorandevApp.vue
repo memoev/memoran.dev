dfdc95b79f5d09995b8f08b14248ff8ab88654c8
Guillermo Villalta <mexcelus@gmail.com>
2019-11-20 11:06:11 -0700

    memorandev components

---
+++ src/components/MemorandevApp/MemorandevApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="memorandev">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemorandevApp',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
