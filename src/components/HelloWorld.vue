5d1a697e0c9bbbceb2ac267008b14d16a0c47b29
Guillermo Villalta <mexcelus@gmail.com>
2019-11-21 15:26:58 -0700

    importing mapstate with title to show

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="hello">
-    <h1><b>{{ msg }}</b></h1>
+    <!-- <h1><b>{{ msg }}</b></h1>
     <h3>Here's where knowledge begins!</h3>
-    <button class="btn btn-success">Sign Up</button>
+    <button class="btn btn-success">Sign Up</button> -->
+    <div class="left">
+      <h1>{{ custom }}</h1>
+    </div>
+    <div class="right">
+
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  computed: {
+    ...mapState({
+      custom: 'title'
+    })
   }
 }
 </script>
