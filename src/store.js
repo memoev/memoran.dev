68ade0a7297ef91d4e362e4b4cbabab8ca2c1285
Guillermo Villalta <mexcelus@gmail.com>
2019-11-21 15:25:26 -0700

    added state, mutations and actions

--- src/store.js
+++ src/store.js
@@ -4,13 +4,24 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-
-    },
-    mutations: {
-
-    },
-    actions: {
-        
+  state: {
+    title: "My Custom Title",
+    categories: [
+      { id: 1, name: "All Notes" },
+      { id: 2, name: "Quick Notes" },
+      { id: 3, name: "Favorites" }
+    ],
+    logIn: false
+  },
+  mutations: {
+    loggingIn(state) {
+      // mutate state
+      state.logIn = !state.logIn;
     }
-})
+  },
+  actions: {
+      toggleLogIn(context) {
+        context.commit('loggingIn');
+      }
+  }
+});
