396ffc4513468c1f0a2cd1dbf418ba9fd1e362a3
Guillermo Villalta <mexcelus@gmail.com>
2019-11-22 17:54:43 -0700

    notes init

---
+++ client/src/components/MemorandevApp/NotesBar/Note.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="note">
+        <h4>{{content}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Notes',
+    props: ["content"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/MemorandevApp/NotesBar/NotesBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="notesbar">
+    <h3>Notes Bar</h3>
+    <div v-for="note in notes" v-bind:key="note._id">
+        <Note v-bind:content="note.content" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Note from './Note.vue'
+
+export default {
+  name: 'NotesBar',
+  components: {
+      Note
+  },
+  props: ['notes']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
