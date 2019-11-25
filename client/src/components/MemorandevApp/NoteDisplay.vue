54b11f13421348b3a992f64b6ea39925a29cc648
Guillermo Villalta <mexcelus@gmail.com>
2019-11-25 00:49:22 -0700

    init remark plugin

--- client/src/components/MemorandevApp/NoteDisplay.vue
+++ client/src/components/MemorandevApp/NoteDisplay.vue
@@ -1,13 +1,41 @@
 <template>
     <div class="note">
+        <button @click="handleClick">Edit Mode</button>
         <h1>{{this.$store.state.selectedNote.title}}</h1>
-        <p>{{this.$store.state.selectedNote.content}}</p>
+        <div v-if="!edit">
+            <div v-html="compiledMarkdown" />
+        </div>
+        <div v-else>
+            <textarea :value="this.$store.state.selectedNote.content" />
+        </div>
     </div>
 </template>
 
 <script>
+const marked = require('marked')
+
 export default {
-    name: 'NoteDisplay'
+    name: 'NoteDisplay',
+    data() {
+        return {
+           edit: false 
+        }
+    },
+    computed: {
+        compiledMarkdown: function () {
+        if (!this.$store.state.selectedNote.content) {
+            return marked('');
+        } else {
+            return marked(this.$store.state.selectedNote.content)
+        }
+        }
+    },
+    methods: {
+        handleClick: function() {
+            this.edit = !this.edit
+        }
+    }
+
 }
 </script>
 
