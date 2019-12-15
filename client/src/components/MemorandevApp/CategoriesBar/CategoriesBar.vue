4d42880a722d0fe6c6dd110e5e10b377ee522298
Guillermo Villalta <mexcelus@gmail.com>
2019-12-15 03:28:21 -0700

    logout from auth0

--- client/src/components/MemorandevApp/CategoriesBar/CategoriesBar.vue
+++ client/src/components/MemorandevApp/CategoriesBar/CategoriesBar.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="categoriesbar">
-    <button @click="toggleLogIn">Log Out</button>
+    <button @click="logout">Log Out</button>
     <div v-for="category in categories" v-bind:key="category.id">
       <Category v-bind:category="category"/>
     </div>
+    <!-- <img :src="$auth.user.picture"> -->
   </div>
 </template>
 
@@ -25,12 +26,22 @@ export default {
   methods: {
     ...mapActions({
       toggleLogIn: 'toggleLogIn'
-    })
+    }),
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+/* img {
+  width: 4em;
+} */
 
 </style>
