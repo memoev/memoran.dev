24994e75f2e2f19d90ca98cff935e7d719b8c6d4
Guillermo Villalta <mexcelus@gmail.com>
2020-09-24 14:38:15 -0600

    test

--- src/main.js
+++ src/main.js
@@ -4,15 +4,19 @@ import store from './store'
 import router from './router'
 
 // Import the Auth0 configuration
-import { domain, clientId } from "../auth_config.json";
+// import { domain, clientId } from "../auth_config.json";
 
 // Import the plugin here
 import { Auth0Plugin } from "./auth";
 
 // Install the authentication plugin here
+
+const dom = process.env.AUTH_DOMAIN;
+const cli = process.env.AUTH_CLIENTID;
+
 Vue.use(Auth0Plugin, {
-  domain,
-  clientId,
+  dom,
+  cli,
   onRedirectCallback: appState => {
     router.push(
       appState && appState.targetUrl
